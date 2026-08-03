fe1cead35b1d3be04a2ce66989e3a2c77d69696d
ziche <zichencaizc@gmail.com>
2026-08-03 21:37:29 +0800

    Initial commit

---
+++ backend/src/main/java/com/astroluxeon/citrussync/CitrusSyncApplication.java
@@ -0,0 +1,13 @@
+package com.astroluxeon.citrussync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitrusSyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitrusSyncApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/astroluxeon/citrussync/CitrusSyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.astroluxeon.citrussync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitrusSyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
